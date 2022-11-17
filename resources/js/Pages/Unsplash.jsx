3af912006e33faf1e323398cacfd7eecc4a3b2cd
Frank Mele <thisisastupidaccount@gmail.com>
2022-11-17 15:12:10 +1100

    Added in the frontend for the unsplash search/list page

--- resources/js/Pages/Unsplash.jsx
+++ resources/js/Pages/Unsplash.jsx
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link, Head } from '@inertiajs/inertia-react';
 
-export default function Hello(props) {
-    return (
-        <>
-            <Head title="Hello" />
-            <div>
-              <input type="text" /> <button type="button">Search</button>
-            </div>
+export default function Unsplash(props) {
+  const [search, setSearch] = useState('');
+  const [disabledFlag, setDisabledFlag] = useState(true);
+  const [items, setItems] = useState([]);
 
-            <ul>
-              <li></li>
-            </ul>
-        </>
-    );
+  /**
+   * Define an abort controller here so we can bail out. Really not needed
+   * as we're just a single component and we're only searching when they click
+   * the button, but here anyway
+   */
+  const abortController = new AbortController();
+
+  useEffect(() => {
+    setDisabledFlag(search.trim() === '')
+  
+    return () => {
+      abortController.abort();
+    };
+  }, [search]);
+
+  const doSearch = async () => {
+    if (disabledFlag) {
+      return;
+    }
+
+    setDisabledFlag(true);
+    
+    try {
+      const rtn = await axios.post('api/search', { search }, { signal: abortController.signal });
+      setItems(rtn.data);
+    } catch (e) {
+
+      /**
+       * Tell someone about it. Who? I've no idea
+       */
+      console.error(e);
+    }
+
+    setDisabledFlag(false);
+  }
+
+  return (
+    <>
+      <Head title="Unsplash" />
+      <div>
+        <p>Search for something</p>
+        <input type="text" value={search} onChange={(e) => (setSearch(e.target.value))} />
+        <button type="button" disabled={disabledFlag} onClick={doSearch}>Search</button>
+      </div>
+
+      <ul>
+        { items.map(x => (<li key={x.unsplash_api_id}><img src={x.thumbnail_url} /></li>) )}
+      </ul>
+    </>
+  );
 }
